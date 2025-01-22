b4c1f0fae839a36d9d9a2cbec32d1370fb6fa6ac
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-22 14:31:15 +0530

    Convert Kilometer to Miles

---
+++ Assessment2/Distance_Conv.java
@@ -0,0 +1,9 @@
+public class distance_Converter {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 0.621371;
+        System.out.print("10.8 kilometers is equal to"+miles+"miles");
+    }
+}
+
+
