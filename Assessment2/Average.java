4bb4289524c0ce6dc9f85d43dbb8496811e0defc
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-22 14:23:40 +0530

    Average Calculation

---
+++ Assessment2/Average.java
@@ -0,0 +1,8 @@
+public class Average {
+    public static void main(String[] args) {
+        int maths = 94, physics = 95, chemistry = 96;
+        double average = (maths + physics + chemistry) / 3.0;
+        System.out.println("Average percent mark in PCM: " + average + "%");
+    }
+}
+
