d578f6f845ea2059fced1252a23ec388a07c35e5
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-23 15:35:33 +0530

    to check leap year

---
+++ Assessment3Level2/leap_year_check.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Level2Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("The year must be 1582 or later.");
+        } else {
+            if (year % 400 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } else if (year % 100 == 0) {
+                System.out.println(year + " is not a Leap Year.");
+            } else if (year % 4 == 0) {
+                System.out.println(year + " is a Leap Year.");
+            } else {
+                System.out.println(year + " is not a Leap Year by second way.");
+            }
+
+	    //this is second part of prog,and is written inside else coz 
+            //need to check >1582
+
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.println(year + " is a Leap Year.");
+            } else {
+                System.out.println(year + " is not a Leap Year.");
+            }
+        }
+
+    }
+}
