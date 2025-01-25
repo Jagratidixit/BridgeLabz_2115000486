d0810526c683340f979ee64e501df718c3f5fba6
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-25 18:02:33 +0530

    to convert 2d to 1d

---
+++ Assessment4Level1/copy_2d_to_1d.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Level1Q9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = sc.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+        System.out.println("Enter the elements of the 2D Array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] array = new int[rows * columns];
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                array[index++] = matrix[i][j];
+            }
+        }
+
+        System.out.println("1D Array:");
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+
+    }
+}
