44e90c66070cd84a190444c73e727005d6e57509
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-25 17:55:04 +0530

    Fizzbuzz program

---
+++ Assessment4Level1/fizzbuzz.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Level1Q10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int number = sc.nextInt();
+
+        if (number < 0) {
+            System.out.println("Invalid number.");
+            System.exit(0);
+        }
+
+        String[] results = new String[number + 1];
+
+        for (int i = 0; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                results[i] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                results[i] = "Fizz";
+            } else if (i % 5 == 0) {
+                results[i] = "Buzz";
+            } else {
+                results[i] = String.valueOf(i);
+            }
+        }
+
+        System.out.println("FizzBuzz Results:");
+        for (int i = 0; i <= number; i++) {
+            System.out.println("Position " + i + " = " + results[i]);
+        }
+
+    }
+}
