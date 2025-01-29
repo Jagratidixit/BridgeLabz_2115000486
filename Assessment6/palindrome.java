606a1813cb805132f84cf04e0c09a335fe4973dc
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-29 21:32:44 +0530

    to check palindrome

---
+++ Assessment6/palindrome.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		boolean res=palindrome(s);
+		if(res)
+		System.out.println("String is palindrome");
+		else
+		System.out.println("String is not palindrome");
+
+	}
+	public static boolean palindrome(String s){
+	    int i=0;
+		int j=s.length()-1;
+		while(i<j){
+		    if(s.charAt(i)!=s.charAt(j)){
+		        return false;
+		    }
+		    i++;
+		    j--;
+		}
+		return true;
+	}
+}
