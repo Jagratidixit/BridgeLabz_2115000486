affdc2013c1a60147f9f7e2e0c75943b1e6b5e03
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-29 21:30:19 +0530

    to find reverse of a string

---
+++ Assessment6/reverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		String newstring="";
+		int i=s.length()-1;
+		while(i>=0){
+		    newstring=newstring+s.charAt(i);
+		    i--;
+		}
+		System.out.println("Reversed String is:"+newstring);
+
+	}
+}
