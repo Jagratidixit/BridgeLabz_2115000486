8b715b9a25e9e7045cb7f4fb4adfdbc891c3c9ae
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-29 21:40:09 +0530

    to toggle case of each character

---
+++ Assessment6/togglecase_characters.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String newstr = "";
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLowerCase(str.charAt(i))) {
+                newstr = newstr + Character.toUpperCase(str.charAt(i));
+            } else {
+                newstr = newstr + Character.toLowerCase(str.charAt(i));
+            }
+        }
+        
+        System.out.println("Modified String: " + newstr);
+    }
+}
