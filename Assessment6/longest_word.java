38e96f40a51197ff5fcd9afaaa77e40568940e6b
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-29 21:35:42 +0530

    to find longest word

---
+++ Assessment6/longest_word.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+
+        String longestWord = "";
+        String array[] = s.split(" ");
+
+        for (String str : array) {
+            if (str.length() > longestWord.length()) {
+                longestWord = str;
+            }
+        }
+
+        System.out.print("Longest word is: " + longestWord);
+    }
+}
+
