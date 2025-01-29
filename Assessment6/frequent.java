7f0cea58750bc2285f96ef7c055186d9a3aee6f4
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-29 22:11:17 +0530

    to find most frequent character

---
+++ Assessment6/frequent.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int freq[]=new int[256];
+        int maxfreq=0;
+        char mostFrequent=' ';
+        for(char ch:str.toCharArray()){
+            freq[ch]++;
+            if(freq[ch]>maxfreq){
+                maxfreq=freq[ch];
+                mostFrequent=ch;
+            }
+        }
+        
+        System.out.println("Most frequent character is: " + mostFrequent);
+    }
+}
+
