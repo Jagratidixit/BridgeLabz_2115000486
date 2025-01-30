99e56b67e6ce86d8fae3905d6d196a2b0769e8e6
JagratiDixit <jagratidixit2010@gmail.com>
2025-01-30 14:03:05 +0530

    to check anagram string

--- Assessment6/Anagram.java
+++ Assessment6/Anagram.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class anagram {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.nextLine();
