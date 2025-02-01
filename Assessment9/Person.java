c724bec81279841449cd1ef7fe01e6326cd7e8a2
JagratiDixit <jagratidixit2010@gmail.com>
2025-02-01 11:50:37 +0530

    Person Details

---
+++ Assessment9/Person.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(Person p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Alice", 25);
+        Person p2 = new Person(p1);
+        p2.display();
+    }
+}
