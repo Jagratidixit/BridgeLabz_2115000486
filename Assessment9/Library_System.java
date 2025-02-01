20d948a78de3c978947556815366c651b811e095
JagratiDixit <jagratidixit2010@gmail.com>
2025-02-01 22:57:48 +0530

    Library Management System

---
+++ Assessment9/Library_System.java
@@ -0,0 +1,52 @@
+class Book {
+    public String ISBN;
+    protected String title;
+    private String author;
+
+    public Book(String ISBN, String title, String author) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void displayBookDetails() {
+        System.out.println("ISBN: " + ISBN);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+
+class EBook extends Book {
+    private double fileSize;
+
+    public EBook(String ISBN, String title, String author, double fileSize) {
+        super(ISBN, title, author);
+        this.fileSize = fileSize;
+    }
+
+    public void displayEBookDetails() {
+        System.out.println("E-Book Details:");
+        System.out.println("ISBN: " + ISBN);
+        System.out.println("Title: " + title);
+        System.out.println("File Size: " + fileSize + " MB");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        EBook ebook = new EBook("978-1234567890", "Java Programming", "Harry Potter", 2.5);
+        ebook.displayEBookDetails();
+
+        ebook.setAuthor("Jagrati Dixit");
+        System.out.println("\nAfter Updating Author:");
+        ebook.displayBookDetails();
+    }
+}
