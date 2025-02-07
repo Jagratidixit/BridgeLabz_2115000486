4b7212487df6dee251c190539b4f3f2815c13352
JagratiDixit <jagratidixit2010@gmail.com>
2025-02-07 22:08:40 +0530

    Smart Home Devices

---
+++ Assessment12/Single_Inheritance/Smart_Phone.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Device {
+    String deviceId;
+    String status;
+    
+    Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+    
+    void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+class Thermostat extends Device {
+    int temperatureSetting;
+    
+    Thermostat(String deviceId, String status, int temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+    
+    @Override
+    void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Thermostat thermostat = new Thermostat("T1000", "Offline", 20);
+        thermostat.displayStatus();
+    }
+}
