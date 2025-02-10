af2a26aa7507f41548951cb68a61a7c164140959
JagratiDixit <jagratidixit2010@gmail.com>
2025-02-10 23:13:15 +0530

    Banking System

---
+++ Assessment13/Banking.java
@@ -0,0 +1,23 @@
+abstract class BankAccount {
+    private int accountNumber;
+    private String holderName;
+    private double balance;
+
+    public BankAccount(int accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    public abstract double calculateInterest();
+}
+
+class SavingsAccount extends BankAccount {
+    public SavingsAccount(int accountNumber, String holderName, double balance) {
+        super(accountNumber, holderName, balance);
+    }
+
+    public double calculateInterest() {
+        return getBalance() * 0.04;
+    }
+}
