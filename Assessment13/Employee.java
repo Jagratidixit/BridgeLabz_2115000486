0f0ffe851c384a90d49a19019ab456df84fdd4a3
JagratiDixit <jagratidixit2010@gmail.com>
2025-02-10 23:09:47 +0530

    Employee Management System

---
+++ Assessment13/Employee.java
@@ -0,0 +1,61 @@
+abstract class Employee {
+    private int employeeId;
+    private String name;
+    private double baseSalary;
+
+    public Employee(int employeeId, String name, double baseSalary) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+
+    public int getEmployeeId() { return employeeId; }
+    public String getName() { return name; }
+    public double getBaseSalary() { return baseSalary; }
+
+    public abstract double calculateSalary();
+
+    public void displayDetails() {
+        System.out.println("ID: " + employeeId + ", Name: " + name + ", Salary: " + calculateSalary());
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    public FullTimeEmployee(int employeeId, String name, double baseSalary) {
+        super(employeeId, name, baseSalary);
+    }
+
+    public double calculateSalary() {
+        return getBaseSalary();
+    }
+}
+
+class PartTimeEmployee extends Employee {
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(int employeeId, String name, double hourlyRate, int hoursWorked) {
+        super(employeeId, name, 0);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+}
+
+interface Department {
+    void assignDepartment(String dept);
+    String getDepartmentDetails();
+}
+
+class EmployeeManagement {
+    public static void main(String[] args) {
+        Employee emp1 = new FullTimeEmployee(101, "Alice", 50000);
+        Employee emp2 = new PartTimeEmployee(102, "Bob", 200, 40);
+
+        emp1.displayDetails();
+        emp2.displayDetails();
+    }
+}
