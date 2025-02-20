9f9acfb1416b5c7e8e98e8d8f0a1ede9f9118265
JagratiDixit <jagratidixit2010@gmail.com>
2025-02-20 23:22:44 +0530

    Reverse a Queue

---
+++ Assessment20/Queue_Interface/Reverse.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ReverseQueue {
+    public static Queue<Integer> reverse(Queue<Integer> queue) {
+        Stack<Integer> stack = new Stack<>();
+        
+        while (!queue.isEmpty()) {
+            stack.push(queue.poll());
+        }
+        
+        while (!stack.isEmpty()) {
+            queue.add(stack.pop());
+        }
+        
+        return queue;
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        System.out.println("Original Queue: " + queue);
+        System.out.println("Reversed Queue: " + reverse(queue));
+    }
+}
